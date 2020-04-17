9d8afece601e25882edef0790cc690a219bb0a0a
longwork <1749560162@qq.com>
2020-04-17 17:25:07 +0800

    second commit

--- nacos-usermanagement/src/test/java/org/student/NacosUsermanagementApplicationTests.java
+++ nacos-usermanagement/src/test/java/org/student/NacosUsermanagementApplicationTests.java
@@ -1,9 +1,11 @@
 package org.student;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,5 +13,11 @@ public class NacosUsermanagementApplicationTests {
 
     @Test
     public void contextLoads(){
+        List<Integer> list = new ArrayList<> ();
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.forEach(System.out::println);
     }
 }
